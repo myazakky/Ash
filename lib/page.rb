45e86df16d4487a08899324ba79da0d6abff3266
myazakky <nimuc@outlook.com>
2022-02-07 09:15:08 +0900

    ちょっとリファクタリング

--- lib/page.rb
+++ lib/page.rb
@@ -14,37 +14,30 @@ class Page
   def analyse_helper(tokens, result, n, by_bracket)
     return [result, n] if tokens.nil? || tokens.empty?
 
-    if (left_bracket? tokens[0]) && (many? :bold?, tokens[1]) && ((many? :white?, tokens[2]) || (many? :decoration?, tokens[2]))
+    if (left_bracket? tokens[0]) && (many? :decoration?, tokens[1]) && ((many? :white?, tokens[2]) || (many? :decoration?, tokens[2]))
+      # Case of decorated notations like [**-- Hi]
       if many? :white?, tokens[2]
         content = analyse_helper(tokens[3..], [], 0, true)
-        read_at = content[1] + n + 1
+        read_at = content[1] + n + 2
       else
         content = analyse_helper(['['] + tokens[2..], [], 0, true)
-        read_at = content[1] + n
-      end
-
-      literal = Literal.new(content[0], Kind::BOLD)
-      analyse_helper(tokens[(read_at + 2)..], result + [literal], read_at + 1, false)
-    elsif (left_bracket? tokens[0]) && (many? :strike?, tokens[1]) && ((many? :white?, tokens[2]) || (many? :decoration?, tokens[2]))
-      if many? :white?, tokens[2]
-        content = analyse_helper(tokens[3..], [], 0, true)
         read_at = content[1] + n + 1
-      else
-        content = analyse_helper(['['] + tokens[2..], [], 0, true)
-        read_at = content[1] + n
       end
+      literal = many?(:bold?, tokens[1]) ? Literal.new(content[0], Kind::BOLD) : Literal.new(content[0], Kind::STRIKE)
 
-      literal = Literal.new(content[0], Kind::STRIKE)
-      analyse_helper(tokens[(read_at + 2)..], result + [literal], read_at + 1, false)
+      analyse_helper(tokens[(read_at + 1)..], result + [literal], read_at + 1, false)
     elsif backquote? tokens[0]
-      next_backquote = tokens[1..].index('`')
-      literal = Literal.new(Literal.new(tokens[1..next_backquote].inject(&:+), Kind::PLAIN), Kind::CODEBLOCK)
-      analyse_helper(tokens[(next_backquote + 2)..], result + [literal], next_backquote + 1, false)
+      # Case of command line like `hello`
+      next_backquote = tokens[1..].index('`') + 1
+      literal = Literal.new(Literal.new(tokens[1..(next_backquote - 1)].inject(&:+), Kind::PLAIN), Kind::CODEBLOCK)
+      analyse_helper(tokens[(next_backquote + 1)..], result + [literal], next_backquote + 1, false)
     elsif (left_bracket? tokens[0]) && (latex? tokens[1]) && (many? :white?, tokens[2])
+      # Case of latex like [$ \LaTex]
       read_at = tokens[3..].index(']') + 3
       literal = Literal.new(tokens[3..(read_at - 1)].inject(&:+), Kind::LATEX)
       analyse_helper(tokens[(read_at + 1)..], result + [literal], read_at + 1, false)
     elsif (left_bracket? tokens[0]) && !tokens[1].nil?
+      # Case of link like [Scrapbox is a God tool]
       content = analyse_helper(tokens[1..], [], 0, true)
       literal = Literal.new(content[0], Kind::LINK)
       read_at = content[1] + n
@@ -52,6 +45,7 @@ class Page
     elsif (right_bracket? tokens[0]) && by_bracket
       [result, n + 1]
     elsif (tokens[0].start_with? 'code:') && result.empty? && !by_bracket
+      # Case of code block like code:example.rb
       literal = Literal.new(tokens.inject(&:+), Kind::CODE, { code: '' })
       [[literal], 0]
     elsif result.size.zero?
@@ -68,18 +62,12 @@ class Page
   def analyse
     @row_lines.inject([]) do |result, row_line|
       tokens = tokenize row_line
-      depth = if many? :white?, tokens[0]
-        tokens[0].count(' ') + tokens[0].count('　')
-      else
-        0
-      end
-      if !result.empty? && result[-1][-1].kind == Kind::CODE && depth > 0
+      depth = many?(:white?, tokens[0]) ? tokens[0].count(' ') + tokens[0].count('　') : 0
+
+      if !result.empty? && !result[-1].empty? && result[-1][-1].kind == Kind::CODE && depth > 0
         result[-1][-1].data[:code] += "#{tokens.inject(&:+)[1..]}\n"
         result
-      elsif !result.empty? &&
-              result[-1][-1].kind == Kind::INDENT &&
-              result[-1][-1].text[0].kind == Kind::CODE &&
-              result[-1][-1].data[:depth] < depth
+      elsif !result.empty? && !result[-1].empty? && result[-1][-1].kind == Kind::INDENT && result[-1][-1].text[0].kind == Kind::CODE && result[-1][-1].data[:depth] < depth
         result[-1][-1].text[0].data[:code] += "#{tokens.inject(&:+)[(result[-1][-1].data[:depth] + 1)..]}\n"
         result
       elsif depth.zero?
