21efc10a9b4d941f804f2c1dcc6dbe721fd00106
myazakky <nimuc@outlook.com>
2022-02-09 19:32:24 +0900

    cssを導入

--- main.rb
+++ main.rb
@@ -5,9 +5,22 @@ require 'json'
 
 project_data = JSON.parse(File.read(ARGF.argv[0]))
 
-html = project_data['pages'].map do |page|
+body = project_data['pages'].map do |page|
   page = Page.new(page['lines'], project_data['name'])
   page.to_html
 end.inject(&:+)
+
+html = ''"
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset='UTF-8'>
+<link type='text/css' rel='stylesheet' href='./statics/style.css'>
+</head>
+<body>
+#{body}
+</body>
+<html>
+"''
 
 File.write("#{project_data['displayName']}.html", html)
